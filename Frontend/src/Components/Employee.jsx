29dccbc071ea972bf3766f2a38460194523a7cd8
Raja Jazib Siddique <rajajazibsiddique@gmail.com>
2025-04-18 13:07:36 +0500

    Building Employee Form

--- Frontend/src/Components/Employee.jsx
+++ Frontend/src/Components/Employee.jsx
@@ -1,5 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const defaultImgSrc = "/src/assets/image_placeholder.png";
+
+const initialFieldValues = {
+  employeeId: 0,
+  employeeName: "",
+  occupation: "",
+  imageName: "",
+  imageSrc: defaultImgSrc,
+  imageFile: null,
+};
 
 export default function Employee() {
-  return <div>Employee</div>;
+  const [values, setValues] = useState(initialFieldValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <p className="lead text-center">An Employee</p>
+      </div>
+      <form autoComplete="off" noValidate>
+        <div className="card">
+          <img src={values.imageSrc} className="card-img-top" />
+          <div className="card-body">
+            <div className="form-group ">
+              <input
+                className="form-control-file"
+                type="file"
+                accept="image/*"
+              />
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="form-group my-3">
+              <input
+                className="form-control"
+                placeholder="Employee Name"
+                name="employeeName"
+                value={values.employeeName}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="form-group my-3">
+              <input
+                className="form-control"
+                placeholder="Employee Occupation"
+                name="occupation"
+                value={values.occupation}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
--- Frontend/src/Components/EmployeeList.jsx
+++ Frontend/src/Components/EmployeeList.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Employee from "./Employee";
 
 const EmployeeList = () => {
-  return <div>EmployeeList</div>;
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <div
+          className="jumbotron jumbotron-fluid py-4"
+          style={{ backgroundColor: "#b7b6b6" }}
+        >
+          <div className="container">
+            <h1 className="display-4 text-center">Fluid jumbotron</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-4 my-4">
+        <Employee />
+      </div>
+      <div className="col-md-8 my-4">List of Employee Records</div>
+    </div>
+  );
 };
 
 export default EmployeeList;
